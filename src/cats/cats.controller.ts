1ee3824c8fad3425a8014f2c0b6a149ab20ae91b
Ch0u3ny1b <chouenyib@gmail.com>
2021-10-17 23:55:20 +0200

    Test pipes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -3,18 +3,21 @@ import {
   Controller,
   ForbiddenException,
   Get,
+  Param,
+  ParseUUIDPipe,
   Post,
 } from '@nestjs/common';
 import { CatsService } from 'src/cats/cats.service';
 import { CreateCatDto } from 'src/cats/dto/create-cat.dto';
 import { Cat } from 'src/cats/interfaces/cat.interface';
+import { ValidationPipe } from 'src/pipes/validation.pipe';
 
 @Controller('cats')
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
-  async create(@Body() createCatDto: CreateCatDto) {
+  async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
     throw new ForbiddenException();
   }
@@ -24,5 +27,10 @@ export class CatsController {
     return this.catsService.findAll();
     // console.warn('object');
     // throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+  }
+
+  @Get(':uuid')
+  async findOne(@Param('uuid', ParseUUIDPipe) uuid: string) {
+    return this.catsService.findOne(uuid);
   }
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -3,7 +3,28 @@ import { Cat } from 'src/cats/interfaces/cat.interface';
 
 @Injectable()
 export class CatsService {
-  private readonly cats: Cat[] = [];
+  private readonly cats: Cat[] = [
+    {
+      age: 3,
+      breed: 'rrrrrrrrrr',
+      name: 'ben',
+    },
+    {
+      age: 2,
+      breed: 'rrrrrrrrrr',
+      name: 'ben',
+    },
+    {
+      age: 1,
+      breed: 'rrrrrrrrrr',
+      name: 'ben',
+    },
+    {
+      age: 4,
+      breed: 'rrrrrrrrrr',
+      name: 'ben',
+    },
+  ];
 
   create(cat: Cat) {
     this.cats.push(cat);
@@ -11,5 +32,9 @@ export class CatsService {
 
   findAll(): Cat[] {
     return this.cats;
+  }
+
+  findOne(id: string) {
+    return this.cats.filter((c: Cat) => c.name === id);
   }
 }
