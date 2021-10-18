4307861270960408de80d185f1198a9636065056
Ch0u3ny1b <chouenyib@gmail.com>
2021-10-18 22:17:02 +0200

    use pipe in entire module

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
 import { HttpExceptionFilter } from 'src/filters/http-exception.filter';
+import { ValidationPipe } from 'src/pipes/validation.pipe';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 
@@ -9,6 +10,10 @@ import { CatsService } from './cats.service';
   providers: [
     CatsService,
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
   ],
 })
 export class CatsModule {}
