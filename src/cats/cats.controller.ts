b3d56b0d00ecd546f18f930965d0c75924806d5a
Ch0u3ny1b <chouenyib@gmail.com>
2021-10-23 23:21:35 +0200

    use gaurd to handle roles

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -6,17 +6,22 @@ import {
   Param,
   ParseUUIDPipe,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { CatsService } from 'src/cats/cats.service';
 import { CreateCatDto } from 'src/cats/dto/create-cat.dto';
 import { Cat } from 'src/cats/interfaces/cat.interface';
+import { Roles } from 'src/decorators/roles.decorator';
+import { RolesGuard } from 'src/guards/roles.guard';
 import { ValidationPipe } from 'src/pipes/validation.pipe';
 
 @Controller('cats')
+@UseGuards(RolesGuard)
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
+  @Roles('admin')
   async create(@Body(new ValidationPipe()) createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
     throw new ForbiddenException();
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { HttpExceptionFilter } from 'src/filters/http-exception.filter';
+import { RolesGuard } from 'src/guards/roles.guard';
 import { ValidationPipe } from 'src/pipes/validation.pipe';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
@@ -13,6 +14,10 @@ import { CatsService } from './cats.service';
     {
       provide: APP_PIPE,
       useClass: ValidationPipe,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
     },
   ],
 })
